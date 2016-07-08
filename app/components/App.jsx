3ac2641aa38a3e467426eb74e2328d113da98520
Filip Rydlo <filip.rydlo@gmail.com>
2016-07-08 17:08:14 +0200

    prepared App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -6,33 +6,20 @@ import {DragDropContext} from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
 import connect from '../libs/connect';
-import Lanes from './Lanes';
-import LaneActions from '../actions/LaneActions';
 
 
-const App = ({LaneActions, lanes}) => {
-	const addLane = () => {
-		LaneActions.create({
-			id: uuid.v4(),
-			name: 'New lane'
-		});
-	};
+const App = ({grids}) => {
 
 	return (
 		<div>
-			<button className="add-lane" onClick={addLane}>+</button>
-			<Lanes lanes={lanes} />
+			<Grids grids={grids} />
 		</div>
 	);
 };
 /* ------------wrong syntax-highlight STOPPER!------------- */
 
 
-export default compose(
-  DragDropContext(HTML5Backend),
-  connect(({lanes}) => ({
-    lanes
-  }), {
-    LaneActions
+export default connect({grids}) => ({
+    grids
   })
-)(App)
+(App)
